e0891e6537d30d97fbdc6dc6adcd18636508d2df
Vinicius vianna <vinicius.vianna.dev@outlook.com>
2020-02-25 14:12:41 -0300

    Update calculator.dart

--- Flutter/calculadora/lib/screens/calculator.dart
+++ Flutter/calculadora/lib/screens/calculator.dart
@@ -23,6 +23,7 @@ class _CalculatorState extends State<Calculator> {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
     return MaterialApp(
+	debugShowCheckedModeBanner: false,
       home: Column(
         children: <Widget>[
           Display(memory.value),
