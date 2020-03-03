6dc5860c3c8de38841538973c61deedadca619be
ViniciusFVianna <vinicius-durap-16@hotmail.com>
2020-03-02 22:10:44 -0300

    Adição da configuração para consumo da API

---
+++ Flutter/pokedex/lib/stores/pokeAPIStore.store.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'pokeapistore.store.g.dart';
+
+class PokeAPIStore = _PokeAPIStoreBase with _$PokeAPIStore;
+
+abstract class _PokeAPIStoreBase with Store {
+  
+}
