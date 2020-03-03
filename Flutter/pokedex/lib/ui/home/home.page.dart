00dc13a3d3f61f384dc0c09cbcac9c638e65e65d
ViniciusFVianna <vinicius-durap-16@hotmail.com>
2020-03-02 21:01:34 -0300

    Adição de dependências
    Adição de dependências.

--- Flutter/pokedex/lib/ui/home/home.page.dart
+++ Flutter/pokedex/lib/ui/home/home.page.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC')
-      ),
+    return Container(
+      
     );
   }
 }
