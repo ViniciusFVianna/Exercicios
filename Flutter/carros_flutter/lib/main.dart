89069c544dd6243d6ff96f77ffff3228faee3d71
Vinicius vianna <vinicius.vianna.dev@outlook.com>
2020-04-10 20:44:39 -0300

    ajustes

--- Flutter/carros_flutter/lib/main.dart
+++ Flutter/carros_flutter/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:carrosflutter/pages/splash/splash_page.dart';
 import 'package:carrosflutter/utils/event_bus.dart';
+import 'package:carrosflutter/utils/firebase.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main(){
+runApp(MyApp());
+initFcm();
+}
 
 class MyApp extends StatelessWidget {
   @override
